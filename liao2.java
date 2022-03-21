4f836a9694b5cd1bb8483faa534cbef248a37ea7
goddessIU <2581606394@qq.com>
2022-03-21 11:01:41 +0800

    到达继承

---
+++ liao2.java
@@ -0,0 +1,52 @@
+public  class liao2 {
+}
+
+//class Person {
+//    private String name;
+//    private int age;
+//    private int birth;
+//
+//    public Person(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    public Person(String name) {
+//        this.name = name;
+//        this.age = 12;
+//    }
+//
+//    public  void setBirth(int year) {
+//        this.birth = year;
+//    }
+//
+//    public int getAge() {
+//        return calcAge(2019);
+//    }
+//
+//    private int calcAge(int currentYear) {
+//        return currentYear - this.birth;
+//    }
+//}
+//
+//class Group {
+//    private String[] names;
+//
+//    public void setNames(String[] names) {
+//        this.names = names;
+//    }
+
+//    public String getNames() {
+//        return this.names[0] + ":" + this.names[1];
+//    }
+//}
+
+class Hello {
+    public void hello() {
+        System.out.println("aaa");
+    }
+
+    public void hello(String name) {
+        System.out.println("aaa" + name);
+    }
+}
